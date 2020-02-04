32df0e6c084d5260ed238ba629b26053c78cf72a
koyger <koygerov.work@gmail.com>
2020-02-04 23:02:41 +0300

    Username generator is added

--- resources/config_inc.php
+++ resources/config_inc.php
@@ -12,3 +12,6 @@ $g_db_table_suffix          = 'mantis';
 $g_default_timezone       = 'Europe/Berlin';
 
 $g_crypto_master_salt     = 'NyPjP3tNiGwJ3uAanq8B5gBTtFeqAAuXz9yeRNVr+7Y=';
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_port = '25';
+$g_smtp_host = 'localhost';
--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -1,4 +1,13 @@
+import string
+import random
+
+
+def random_username(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
 def test_signup_new_account(app):
-    username = "user1"
+    username = random_username("User_", 10)
     password = "test"
     app.james.ensure_user_exists(username, password)
