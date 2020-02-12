832509e6393d73652791f57d65d220663910329d
koyger <koygerov.work@gmail.com>
2020-02-12 12:38:33 +0300

    In soap.py used url from target.json self.app.base_url

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,7 +9,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-2.23.0/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url +"/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,7 +17,7 @@ class SoapHelper:
             return False
 
     def projects_list(self, username, password):
-        client = Client("http://localhost/mantisbt-2.23.0/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url +"/api/soap/mantisconnect.php?wsdl")
         pr_list = []
         try:
             pr_soap_list = client.service.mc_projects_get_user_accessible(username, password)
